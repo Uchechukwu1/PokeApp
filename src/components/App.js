b3d09a4c6131fb6d77dce87c5b1dd53130620465
uche onuekwusi <ucheonuekwusi1234@gmail.com>
2019-10-08 22:22:41 +0100

    This commit is the first draft of the pokeApp

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Nav from "./Nav.js";
+import Header from "./Header.js";
+
+function App() {
+  return (
+    <div className="body">
+      <Nav />
+      <Header />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <h1>Welcome to PokeApp!</h1>
+      <br /> <br />
+      <button className="button">Go!</button>
+    </div>
+  );
+};
+
+export default Header;
