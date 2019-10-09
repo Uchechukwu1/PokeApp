c256357d63ae2550840e301044ac6cba1e5081af
uche onuekwusi <ucheonuekwusi1234@gmail.com>
2019-10-09 13:57:40 +0100

    This commit is editing the pokemonList component

--- src/components/Pokemon_list.js
+++ src/components/Pokemon_list.js
@@ -5,31 +5,83 @@ class PokemonList extends Component {
   constructor() {
     super();
     this.state = {
-      pokemon: []
+      limit: 10,
+      query: "",
+      total: 0,
+      pokemon: [],
+      next: null,
+      previous: null,
+      filteredResults: []
     };
   }
 
   componentDidMount() {
-    for (let i = 1; i < 10; i++) {
-      fetch(`https://pokeapi.co/api/v2/pokemon/${i}`)
-        .then(response => response.json())
-        .then(responseData => {
-          this.setState({ pokemon: responseData });
-        })
-
-        .catch(error => {
-          console.log("error fetching and parsing data", error);
-        });
-    }
+    this.retrieveList();
   }
 
+  retrieveList = url => {
+    const { limit, query } = this.state;
+    fetch(
+      url || `https://pokeapi.co/api/v2/pokemon/?offset=${0}&limit=${limit}`
+    )
+      .then(response => response.json())
+      .then(responseData => {
+        this.setState({
+          pokemon: responseData.results,
+          filteredResults: this.filterResults(query, responseData.results),
+          next: responseData.next,
+          previous: responseData.previous,
+          total: responseData.count
+        });
+      })
+
+      .catch(error => {
+        console.log("error fetching and parsing data", error);
+      });
+  };
+  openDetails = pokemon => {
+    // open details page the selected pokemon
+    this.props.history.push("/pokemon/details/", pokemon);
+  };
+  goToNext = () => {
+    this.retrieveList(this.state.next);
+  };
+  goToPrevious = () => {
+    this.retrieveList(this.state.previous);
+  };
+  filterResults = (query, list) => {
+    const ex = new RegExp(`^${query}`, "igm");
+    return list.filter(x => ex.test(x.name));
+  };
+  filter = e => {
+    const { pokemon } = this.state;
+    this.setState({
+      query: e.target.value,
+      filteredResults: this.filterResults(e.target.value, pokemon)
+    });
+  };
   render() {
-    console.log(this.state.pokemon);
+    const {
+      next,
+      previous,
+      total,
+      query,
+      filteredResults,
+      pokemon
+    } = this.state;
     return (
       <div>
-        {this.state.pokemon.map((pokemons, key) => (
-          <div>{pokemons.name}</div>
+        <div>
+          <input name="search" value={query} onChange={this.filter} />
+        </div>
+        {((query && filteredResults) || pokemon).map(pokemon => (
+          <div key={pokemon.name} onClick={() => this.openDetails(pokemon)}>
+            {pokemon.name}
+          </div>
         ))}
+        {previous && <button onClick={this.goToPrevious}>previous</button>}
+        {next && <button onClick={this.goToNext}>next</button>}
+        <span>there are {total} pokemons</span>
       </div>
     );
   }
--- src/components/pokemon.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-const Pokemon = props => {
-  console.log(props.url);
-  return <li>{props.url}</li>;
-};
-
-export default Pokemon;
